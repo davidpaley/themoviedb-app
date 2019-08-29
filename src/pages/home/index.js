3d7bffe14903e52c405a16f6976103bad962cf65
davidpaley <david.paleyy@gmail.com>
2019-08-29 16:30:15 -0300

    geting data from the API

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getDiscoverMovies, getConfiguration } from '../../services/movies';
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+  const [config, setConfig] = useState({});
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    getDiscoverMovies().then(dataResponse => {
+      setMovies(dataResponse.results);
+    });
+    getConfiguration().then(response => {
+      setConfig(response);
+    })
+  }, []);
   return (
     <div>
       <h1>This is the home</h1>
+      {config && <span>config set</span>}
+      {movies && <span>movies set</span>}
     </div>
   );
 }
---
+++ src/services/movies.js
@@ -0,0 +1,32 @@
+import request from '../utils/request';
+import {
+  setOnStorage,
+  getFromStorage,
+} from '../utils/localStorage'; 
+
+const URL = 'https://api.themoviedb.org/3';
+const API_KEY_LABEL = 'api_key=69688a4a8e7f1a8912e2989cc7be5d27';
+
+export async function getConfiguration() {
+  return new Promise(function(resolve, reject) {
+    const config = getFromStorage('moviesConfig');
+    if (!config) {
+      return request(`${URL}/configuration?${API_KEY_LABEL}`, {
+        method: 'GET',
+      })
+      .then(responseData => {
+        const configImg = responseData.images;
+        setOnStorage('moviesConfig', configImg);
+        resolve(configImg);
+      })
+      .catch(err => reject(err));
+    }
+    resolve(config);
+  });
+};
+
+export async function getDiscoverMovies() {
+  return request(`${URL}/discover/movie?sort_by=popularity.desc&${API_KEY_LABEL}`, {
+    method: 'GET',
+  });
+}
