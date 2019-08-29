adbd340f1c0411e38c8460af90a4fcd330f22feb
davidpaley <david.paleyy@gmail.com>
2019-08-29 15:18:37 -0300

    creating project structure

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import MovieDetail from './pages/detail';
+import Home from './pages/home';
+import Header from './components/header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Router>
+        <div>
+          <Route path="/" exact component={Home} />
+          <Route path="/movie/:id" component={MovieDetail} />
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
